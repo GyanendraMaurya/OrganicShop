90652af8a8fef599c79ff2acba7459d36715cb3c
Gyanendra Maurya <gmaurya973@gmail.com>
2021-01-23 20:27:29 +0530

    check out module my orders and admin order page updated

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 import { ShoppingCartService } from './shopping-cart.service';
 
 @Injectable({
@@ -9,9 +10,20 @@ export class OrderService {
 
   constructor(private shoppingCartService: ShoppingCartService, private db: AngularFireDatabase) { }
 
+  getOrders() {
+    return this.db.list('/orders').valueChanges();
+  }
+
+  getOrdersByUser(userId: string) {
+    return this.db.list('/orders', ref => ref.orderByChild('userId').equalTo(userId)).valueChanges();
+
+  }
+
   async placeOrder(order) {
     let result = await this.db.list('/orders').push(order);
     this.shoppingCartService.clearCart();
     return result
   }
+
+
 }
